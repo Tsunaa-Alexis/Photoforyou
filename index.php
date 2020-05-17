3a06c60d86c40151f35eccee08890548078c6abf
Alexis <55393916+Tsunaa-Alexis@users.noreply.github.com>
2020-05-17 18:53:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 </head>
 <body>
 	<div class="container text-center">
-
+<!-- TEEEEEEEEEEEEEEEEEEEESSSST -->
 		<div class="py-5 text-center">
       <img class="d-block mx-auto mb-2" src="images/logo.png" alt="logo photoforyou" width="170" height="115">
     	<h1 class="display-5">PhotoForYou</h1>
